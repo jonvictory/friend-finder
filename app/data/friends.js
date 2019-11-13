74de3b6283071baadb4ecf55df6f2aea3c6e7877
jonhuynh <jonvictory@gmail.com>
2019-11-12 17:30:46 -0700

    fixed routing problems. Preparing to work on logic and layout.

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,15 +5,25 @@
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var tableArray = [
+var friendArray = [
     {
-      customerName: "Ahmed",
-      customerEmail: "ahmed@example.com",
-      customerID: "afhaque89",
-      phoneNumber: "000-000-0000"
-    }
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = tableArray;
+  module.exports = friendArray;
   
